2b493debdf61e2124e67b7c94867b71adfb5133d
sklagat45 <sklagat46@gmail.com>
2021-01-11 19:20:39 +0300

    feature-find-value

--- src/main.kt
+++ src/main.kt
@@ -29,7 +29,6 @@ fun ArrayList<Int>.findIndices(value: Int): IntRange? {
 fun main(){
     val sorted = arrayListOf(1,3,6,42,741,20202)
     println(sorted.binarySearch (741))
-    println(sorted.reverse())
 
 
     val array = arrayListOf(1,2,2,2,2,3,3,3,4,5,5)
